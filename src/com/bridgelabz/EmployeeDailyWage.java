d1a0733610646b2e01c370a2c3456066c2c51505
ram-221 <rameshmalli857.com>
2022-10-26 21:34:36 +0530

    Calculate Daily Employee Wage

---
+++ src/com/bridgelabz/EmployeeDailyWage.java
@@ -0,0 +1,28 @@
+package com.bridgelabz;
+
+public class EmployeeDailyWage {
+
+	public static void main(String[] args) {
+		
+		//Constants
+		int IS_FULL_TIME = 1;
+		int EMP_RATE_PER_HOUR = 20;
+		
+		//Variables
+		int empHrs=0;
+		int empWage=0;
+		
+		//Computation
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck==IS_FULL_TIME) {
+			empHrs=8;
+		System.out.println("Employee is Present");
+		}
+		else {
+			empWage=0;
+			System.out.println("Employeeis Abscent");
+		}
+		empWage=empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage:" +empWage);
+	}
+}
